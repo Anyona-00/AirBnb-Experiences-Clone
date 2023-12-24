cb1df9ceb72544101626fd26cda4ee2220cd9d85
Anyona-00 <125492744+Anyona-00@users.noreply.github.com>
2023-12-24 10:26:53 +0300

    setup 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Navbar from './Navbar'
+import Hero from './Hero'
 import './index.css'
 
+
+function Page (){
+    return (
+      <div>
+        <Navbar />
+        <Hero />
+      </div>
+    );
+  }
+  
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Page />
 )
