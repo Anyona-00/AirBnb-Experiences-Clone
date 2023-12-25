7eb3e0fee7e6eb92495ae1f12c6937a2c8f3c960
Anyona-00 <125492744+Anyona-00@users.noreply.github.com>
2023-12-25 07:43:04 +0300

    static done

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -8,7 +8,8 @@ export default function Hero(){
         <div className="hero-text">
         <h1>Online Experiences</h1>
         <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
-</p></div>
+</p>
+</div>
        </div>
     )
 }
